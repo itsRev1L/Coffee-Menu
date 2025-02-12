66acdde29e353a2f97923def2226e06f77f0e004
Am1r <158845341+itsRev1L@users.noreply.github.com>
2025-02-12 20:28:52 +0330

    added a simple router to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MenuPage from "./Pages/MenuPage";
 import QuestionPage from "./Pages/QuestionPage";
-let active = false;
+// let active = false;
 function App() {
-  return <>{active ? <MenuPage /> : <QuestionPage />}</>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MenuPage />} />
+          <Route path="/QuestionPage" element={<QuestionPage />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
