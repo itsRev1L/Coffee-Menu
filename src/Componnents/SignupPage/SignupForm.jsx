da083af6ccf7c48658b47646a67dd2ba7400ad3d
Am1r <158845341+itsRev1L@users.noreply.github.com>
2025-05-09 16:27:39 +0330

    Added signup page

---
+++ src/Componnents/SignupPage/SignupForm.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import "../LoginPage/LoginForm.css";
+import { Container, Row, Col } from "reactstrap";
+
+const SignupForm = () => {
+  return (
+    <Container className="login-container rtl">
+      <Row className="justify-content-center">
+        <Col md={12} lg={5} xs={12}>
+          <h2 className="login-title text-center my-4">
+            اطلاعات زیر را وارد کنید
+          </h2>
+          <form className="login-form-box p-4 shadow rounded">
+            <Row className="mb-3">
+              <Col>
+                <input
+                  type="txt"
+                  placeholder="نام"
+                  className="login-input form-control"
+                  required
+                />
+              </Col>
+              <Col>
+                <input
+                  type="txt"
+                  placeholder="نام خانوادگی"
+                  className="login-input form-control"
+                  required
+                />
+              </Col>
+            </Row>
+
+            <Row className="mb-3">
+              <Col>
+                <input
+                  type="tel"
+                  placeholder="شماره تلفن"
+                  className="login-input form-control"
+                  required
+                  dir="rtl"
+                />
+              </Col>
+            </Row>
+            <Row className="mb-3">
+              <Col>
+                <input
+                  type="email"
+                  placeholder="ایمیل"
+                  className="login-input form-control"
+                  required
+                />
+              </Col>
+            </Row>
+            <Row className="mb-3">
+              <Col>
+                <input
+                  type="password"
+                  placeholder="رمز عبور"
+                  className="login-input form-control"
+                  required
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Col>
+                  <input
+                    type="password"
+                    placeholder="تکرار رمز عبور"
+                    className="login-input form-control"
+                    required
+                  />
+                </Col>
+              </Col>
+            </Row>
+            <Col>
+              <button type="submit" className="login-button btn w-100 mt-3">
+                ورود
+              </button>
+            </Col>
+          </form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default SignupForm;
--- src/Pages/SignupPage.jsx
+++ src/Pages/SignupPage.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import SignupForm from "../Componnents/SignupPage/SignupForm";
+import { Container, Row, Col } from "reactstrap";
 
-const SignupPage = () => {
-  return <div>SignupPage</div>;
-};
+function SignupPage() {
+  return (
+    <Container>
+      <Row className="d-flex justify-content-center align-items-center  text-center ">
+        <Col>
+          <SignupForm />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
 
 export default SignupPage;
